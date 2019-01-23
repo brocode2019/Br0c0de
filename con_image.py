16a4099c1a6040b58b318f82a497e88d0c9972e3
Karthikeya GS <39125026+karthikeya-gs07@users.noreply.github.com>
2019-01-23 23:50:42 +0530

    Add files via upload

---
+++ con_image.py
@@ -0,0 +1,35 @@
+import sys
+import os
+from argparse import ArgumentParser
+from os.path import basename
+
+
+from classes.inference.Sampler import *
+
+def arguements():
+  parser = ArgumentParser()
+  parser.add_argument('--png_path',dest='png_path', help='png_path to generate the code',required=True)
+  parser.add_argument('--output_folder', dest='output_path',required=True)
+  parser.add_argument('--model_json_file', dest='json_file',required=True)
+  parser.add_argument('--model_weights_file', dest='weights_file',required=True)
+  parser.add_argument('--style',dest='style',default='default')
+  parser.add_argument('--verbose',dest='verbose', default=1)
+
+  return parser
+
+def main():
+    parser = arguements()
+    values= parser.parse_args()
+    png_path = values.png_path
+    output_path = values.output_path
+    style=values.style
+    json_file = values.json_file
+    weights_file = values.weights_file
+    verbose = values.verbose
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    sampler = Sampler(model_json_path=json_file,
+                      model_weights_path = weights_file)
+    sampler.convert_single_image(output_path, png_path=png_path, print_generated_output=verbose, style=style)
+if __name__ == "__main__":
+  main()
